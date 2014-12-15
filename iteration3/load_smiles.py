a0f13a7f4a2109e787e9d9b7baaf4dae62d0e1e2
msit18 <msit@wellesley.edu>
2014-12-15 15:38:47 -0500

    comments

--- iteration3/load_smiles.py
+++ iteration3/load_smiles.py
@@ -1,3 +1,6 @@
+#loads smile database and takes a logistical regression of it to create a generalized image of a smile
+#used in the smile detector
+
 import sklearn
 from sklearn.datasets import *
 from scipy.io import loadmat
